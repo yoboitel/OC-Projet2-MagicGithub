62c97ae3b3d19f75bf058f05bab07f47ad393c7f
Yohan <yohan@captiv.eu>
2019-07-18 11:20:28 +0200

    Project Compile Sucessfully and unit test successfully passed too

---
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -0,0 +1,30 @@
+package com.openclassrooms.magicgithub.repository;
+
+import com.openclassrooms.magicgithub.api.ApiService;
+import com.openclassrooms.magicgithub.model.User;
+
+import java.util.List;
+
+public class UserRepository {
+
+    private final ApiService apiService; // TODO: A utiliser
+
+    public UserRepository(ApiService apiService) {
+        this.apiService = apiService;
+    }
+
+    public List<User> getUsers() {
+        // TODO: A modifier
+        return this.apiService.getUsers();
+    }
+
+    public void generateRandomUser() {
+        // TODO: A modifier
+        this.apiService.generateRandomUser();
+    }
+
+    public void deleteUser(User user) {
+        // TODO: A modifier
+        this.apiService.deleteUser(user);
+    }
+}
